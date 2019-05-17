bd58bf259a3e3e4a5d9d3fa352f2179432af8ae1
Joshua Rhodes <grimscar@users.noreply.github.com>
2019-05-17 02:41:17 -0400

    Update app.js
    Copied Tamik's new logic to test branch to play with.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,40 @@
 
-
-
-/**
- * Parser functions
- * takes in input in the first text box and sets up the table with the values seen
- * in the source
- */
-
-document.querySelector('#nButton').addEventListener('click', function(event){
-    document.querySelector('#num').innerHTML += 1;
-    switchInputToTable();
+// Event listener for the journal input button
+document.querySelector('#bJournal').addEventListener('click', function(event){
+    //get the text from the input field
+    let inputData = document.querySelector("#input-journal").value;
+    
+    //This function returns the table want to show
+    let childAppend = makeTable(inputData);
+    document.querySelector('body').appendChild(childAppend);
+    toggleInputTableVisibility();
+    
 });
 
-function switchInputToTable(){
-    let inp = document.querySelector('#input-field');
-    let otb = document.querySelector('#output-table');
+//change the table to show instead of the input field
+function toggleInputTableVisibility(){
+    
+    //hide the input and button while showing the table
+    document.getElementById('input-journal').classList.add("hide");
+    document.getElementById('output-journal').classList.remove("hide");
+    document.getElementById('bjournal').classList.add("hide");
+}
 
-    inp.classList.toggle('hide');
-    otb.classList.toggle('hide');
+function makeTable(str){
+    let newTable = document.createElement("table");
+    // https://stackoverflow.com/questions/14643617/create-table-using-javascript
+    
+    //stand in until we get regex
+    //the next few lines set each word in the input as a separate td
+    let array = str.split(" ");
+    array.forEach(element => {
+        let cell = document.createElement("td");
+        let cellText = document.createTextNode(element);
+        cell.appendChild(cellText);
+        newTable.appendChild(cell);
+    });
+
+    return newTable;
 }
 
 /* Item list that needs REGEX setup to pull from.
