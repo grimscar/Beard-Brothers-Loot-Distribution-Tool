5644dbafe3876f7560159c0b4c8611f66509df32
Eric <eric.whitfield@live.com>
2019-05-18 09:30:03 -0400

    feat Geneate table based on new lines
    Added logic to go through the input field and generate the table, making each new row for each newline character

--- js/app.js
+++ js/app.js
@@ -27,14 +27,7 @@ function makeTable(str){
     // https://stackoverflow.com/questions/14643617/create-table-using-javascript
     
     //stand in until we get regex
-    let array = str.split(" ");
-
-    //array for testing
-    array = [
-        ['a', 'b', 'c', 'd'],
-        [1, 2, 3, 4],
-        ['apple', 'cat', 'fox', 'tree']
-    ]
+    let array = parse(str);
 
     //the next few lines set each word in the input as a separate td
     //it expects array to be a 2 dimensional array
@@ -42,13 +35,36 @@ function makeTable(str){
         newTable.appendChild(document.createElement("tr"));
         element.forEach(el => {
             let cell = document.createElement("td");
-            let cellText = document.createTextNode(element);
+            let cellText = document.createTextNode(el);
             cell.appendChild(cellText);
             newTable.appendChild(cell);
         })
     });
 
     return newTable;
+}
+
+//takes in a string and returns a 2d array based on parsing out each line as a separate row
+function parse(str){
+    debugger;
+    //what we are returning
+    let out = [];
+
+    //what we are parsing against
+    let p = " ";
+    
+    // make the initial array by spliting each line
+    out = str.split('\n');
+
+    //go through the array and split each element into its own array
+    for (let i = 0; i < out.length; i++){
+        let tempString = out[i];
+        out[i] = tempString.split(p);
+    }
+    /*out.forEach(element => {
+        element.replaceWith(element.split(p));
+    });*/
+    return out;
 }
 
 function addTable(tbl)
@@ -64,3 +80,36 @@ function addTable(tbl)
  * else quantity + #
  * 
  */
+
+
+/* Item list that needs REGEX setup to pull from.
+
+^You see: (.+) : (\d+)$   This regex would match line 1
+^You see: (.+)$           This regex would match line 2
+
+args[0] = the first () capture and args[1] would be the 2nd () catpure.
+
+Line 1 is for multiple items
+Line 2 is for single items.
+
+This raw paste could contain several single items as well such as research notes, they don't stack
+thus will never have a " : #" format on them.
+
+You see: Poison Aspect Core : 2
+You see: Void Aspect Core
+You see: Water Aspect Core
+You see: Fire Aspect Core
+You see: Air Aspect Core : 3
+You see: Earth Aspect Core
+You see: Poison Aspect Core : 2
+You see: Shadow Aspect Extract
+You see: shadow phylactery
+You see: water phylactery
+You see: Command Aspect Extract
+You see: Eldritch Aspect Extract
+You see: Water Aspect Extract
+You see: discordance skill mastery scroll
+You see: fishing skill mastery scroll
+You see: carpentry skill mastery scroll
+You see: remove trap skill mastery scroll : 2
+*/
